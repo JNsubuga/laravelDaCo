e0bd1f2b2b1c9a9e56a99880cfc93503d63a24ab
JNsubuga <jkiwanjago@gmail.com>
2022-10-31 23:25:55 +0300

    v1.0.1

---
+++ resources/views/superadmins/index.blade.php
@@ -0,0 +1,15 @@
+<x-superadmin-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('SuperAdmin Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-4 w-full">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mx-2">
+            <div class="p-4 bg-white border-b border-gray-200">
+                You're logged in!
+            </div>
+        </div>
+    </div>
+</x-superadmin-layout>
